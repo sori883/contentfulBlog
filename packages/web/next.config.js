c7c9ca42bdbb767f1bb35ce7db5c1ca32a58a9c7
sorinaji <37400002+sori883@users.noreply.github.com>
2022-12-03 01:31:59 +0900

    publicにイラン

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -16,7 +16,6 @@ const nextConfig =  {
   publicRuntimeConfig: {
     APP_ROOT_URL: process.env.APP_ROOT_URL, 
     BLOG_TITLE: process.env.BLOG_TITLE,
-    CONTENTFUL_GRAPHQL_ENDPOINT: process.env.CONTENTFUL_GRAPHQL_ENDPOINT,
     GCS_URL: process.env.GCS_URL,
     TWITTER_ID: process.env.TWITTER_ID,
     GOOGLE_TAG_MANAGER_ID: process.env.GOOGLE_TAG_MANAGER_ID
--- packages/web/src/graphql/client.ts
+++ packages/web/src/graphql/client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import getConfig from "next/config";
 
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
-const CONTENTFUL_GRAPHQL_ENDPOINT = serverRuntimeConfig.CONTENTFUL_GRAPHQL_ENDPOINT || publicRuntimeConfig.CONTENTFUL_GRAPHQL_ENDPOINT;
+const { serverRuntimeConfig } = getConfig();
+const CONTENTFUL_GRAPHQL_ENDPOINT = serverRuntimeConfig.CONTENTFUL_GRAPHQL_ENDPOINT;
 
 export const client = new ApolloClient({
   uri: `${CONTENTFUL_GRAPHQL_ENDPOINT}`,
