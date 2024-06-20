8ca9798d3f991118e577d08d781bcb5e36eb1d9a
sorinaji <37400002+sori883@users.noreply.github.com>
2024-06-20 22:39:04 +0900

    mdxプラグインのみ更新してSSG

--- apps/blog/vite.config.ts
+++ apps/blog/vite.config.ts
@@ -1,8 +1,22 @@
 import pages from "@hono/vite-cloudflare-pages";
 import adapter from "@hono/vite-dev-server/cloudflare";
+import ssg from "@hono/vite-ssg";
 import honox from "honox/vite";
 import client from "honox/vite/client";
 import { defineConfig } from "vite";
+
+const entry = "./app/server.ts";
+
+// mdx
+import mdx from "@mdx-js/rollup";
+import recmaExportFilepath from "recma-export-filepath";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+import rehypeMdxCodeProps from "rehype-mdx-code-props";
+import rehypeMdxImportMedia from "rehype-mdx-import-media";
+import rehypeMermaid from "rehype-mermaid";
 
 export default defineConfig(({ mode }) => {
   if (mode === "client") {
@@ -16,10 +30,30 @@ export default defineConfig(({ mode }) => {
     };
   } else {
     return {
+      build: {
+        emptyOutDir: false,
+      },
       plugins: [
         honox({ devServer: { adapter } }),
-        pages()
-      ]
+        pages(),
+        mdx({
+          // TODO provider追加
+          jsxImportSource: "hono/jsx",
+          remarkPlugins: [
+            remarkFrontmatter,
+            remarkMdxFrontmatter,
+            remarkGfm
+          ],
+          rehypePlugins: [
+            rehypeHighlight,
+            rehypeMdxCodeProps,
+            rehypeMdxImportMedia,
+            rehypeMermaid
+          ],
+          recmaPlugins: [recmaExportFilepath],
+        }),
+        ssg({ entry }),
+      ],
     };
   }
 });
