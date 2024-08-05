9824732ca84d28a090ad1c20d79cd682f3cf6659
sorinaji <37400002+sori883@users.noreply.github.com>
2024-08-05 21:55:25 +0900

    Update vite.config.ts

--- apps/blog/vite.config.ts
+++ apps/blog/vite.config.ts
@@ -87,7 +87,7 @@ export default defineConfig(({ mode }) => {
               const destPath = normalizePath(
                 path
                   .relative(__dirname, fullPath)
-                  .replaceAll("app\\routes\\posts\\", ""),
+                  .replaceAll("app/routes/posts/", ""),
               );
               return destPath;
             },
