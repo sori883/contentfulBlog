e3be93fae7a6e635d238561904b994f2a32fbce6
sorinaji <37400002+sori883@users.noreply.github.com>
2024-10-22 22:25:10 +0900

    Delete index.ts

--- apps/blog/app/routes/.well-known/discord/index.ts
+++
@@ -1,7 +0,0 @@
-import { createRoute } from "honox/factory";
-
-export default createRoute((c) => {
-  // ファイル書き出しするため、改行必須
-  const robotsTxt = `dh=1eb6b52002473dc923cfc58bc269805735dd4d1d`;
-  return c.text(robotsTxt, 200, {});
-});
