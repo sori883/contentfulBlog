bc4e2c05f88c682e020ba776f72af48c3407f687
sorinaji <37400002+sori883@users.noreply.github.com>
2025-01-25 14:38:49 +0900

    fix mermaid

--- apps/blog/vite.config.ts
+++ apps/blog/vite.config.ts
@@ -10,8 +10,6 @@ import recmaExportFilepath from "recma-export-filepath";
 import rehypeHighlight from "rehype-highlight";
 import rehypeMdxCodeProps from "rehype-mdx-code-props";
 import rehypeMdxImportMedia from "rehype-mdx-import-media";
-import rehypeMermaid from "rehype-mermaid";
-import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkGfm from "remark-gfm";
@@ -56,7 +54,6 @@ export default defineConfig(({ mode }) => {
             rehypeHighlight,
             rehypeMdxCodeProps,
             rehypeMdxImportMedia,
-            rehypeMermaid,
             rehypeSlug,
           ],
           recmaPlugins: [recmaExportFilepath],
